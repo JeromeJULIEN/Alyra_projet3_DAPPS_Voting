c326c6d0a1d9ca759947cfcae97cdc60f8a4c6df
JeromeJULIEN <102427619+JeromeJULIEN@users.noreply.github.com>
2022-11-21 12:02:09 +0100

    add readme deploiement goerli

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Alyra, project 3 : DAPPS voting
+
+contract address on GOERLI : 0x24E28A73a080A66e497C20826887d4BDeadfCFF6
--- truffle/truffle-config.js
+++ truffle/truffle-config.js
@@ -1,3 +1,5 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+require('dotenv').config();
 /**
  * Use this file to configure your truffle project. It's seeded with some
  * common settings for different networks and features like migrations,
@@ -68,6 +70,10 @@ module.exports = {
      host: "127.0.0.1",     // Localhost (default: none)
      port: 8545,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
+    },
+    goerli:{
+      provider : function(){return new HDWalletProvider({mnemonic:{phrase:`${process.env.MNEMONIC}`},providerOrUrl:`https://goerli.infura.io/v3/${process.env.INFURA_ID}`})},
+      network_id:5,
     },
     //
     // An additional network, but with some advanced options…
